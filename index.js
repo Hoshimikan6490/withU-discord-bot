dd25e6069263e0c11eb892a2a054e21fd6c14c2f
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-11-02 16:01:38 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client, Partials, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+require("dotenv").config();
