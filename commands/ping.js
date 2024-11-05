b3843194b0aae760b8a361a40e5476476fce5354
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-11-05 19:07:16 +0900

    remove error system

--- commands/ping.js
+++ commands/ping.js
@@ -2,23 +2,17 @@ module.exports = {
   name: "ping",
   description: "BotのPingを測定します。",
   run: async (client, interaction) => {
-    try {
-      let sent = await interaction.reply({
-        content: "🔄️　計測中…",
-        fetchReply: true,
-      });
+    let sent = await interaction.reply({
+      content: "🔄️　計測中…",
+      fetchReply: true,
+    });
 
-      interaction.editReply(
-        `# Ping計測結果
+    interaction.editReply(
+      `# Ping計測結果
         - WebsocketのPing: \`${Math.abs(client.ws.ping)}ms\`.
         - APIのLatency: \`${
           sent.createdTimestamp - interaction.createdTimestamp
         }ms\`.`
-      );
-    } catch (err) {
-      err.id = "ping";
-      const errorNotification = require("../errorFunction.js");
-      errorNotification(client, interaction, err);
-    }
+    );
   },
 };
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,72 +2,67 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 require("dotenv").config();
 
 module.exports = async (client, message) => {
-  try {
-    if (message.author.bot) return;
+  if (message.author.bot) return;
 
-    let myPermissions = message.guild.members.me
-      .permissionsIn(message.channel)
-      .toArray();
-    let conditions = [
-      "ViewChannel",
-      "SendMessages",
-      "ManageMessages",
-      "EmbedLinks",
-      "AttachFiles",
-    ];
-    for (const key in conditions) {
-      if (!myPermissions.includes(conditions[key])) {
-        return;
-      }
+  let myPermissions = message.guild.members.me
+    .permissionsIn(message.channel)
+    .toArray();
+  let conditions = [
+    "ViewChannel",
+    "SendMessages",
+    "ManageMessages",
+    "EmbedLinks",
+    "AttachFiles",
+  ];
+  for (const key in conditions) {
+    if (!myPermissions.includes(conditions[key])) {
+      return;
     }
+  }
 
-    //メッセージ展開
-    const MESSAGE_URL_REGEX =
-      /https?:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)/g;
-    const matches = MESSAGE_URL_REGEX.exec(message.content);
-    if (matches) {
-      const [url, guildId, channelId, messageId] = matches;
-      try {
-        const channel = await client.channels.fetch(channelId);
-        const fetchedMessage = await channel.messages.fetch(messageId);
+  //メッセージ展開
+  const MESSAGE_URL_REGEX =
+    /https?:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)/g;
+  const matches = MESSAGE_URL_REGEX.exec(message.content);
+  const [url, guildId, channelId, messageId] = matches;
+  const activeGuildIDs = process.env.activeGuildIDs;
+  if (matches && guildId == activeGuildIDs) {
+    try {
+      const channel = await client.channels.fetch(channelId);
+      const fetchedMessage = await channel.messages.fetch(messageId);
 
-        let buttons = new ActionRowBuilder().addComponents(
-          new ButtonBuilder()
-            .setLabel("メッセージを見る")
-            .setURL(fetchedMessage.url)
-            .setStyle(ButtonStyle.Link),
-          new ButtonBuilder()
-            .setCustomId("cancel")
-            .setEmoji("🗑️")
-            .setStyle(ButtonStyle.Secondary)
-        );
+      let buttons = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel("メッセージを見る")
+          .setURL(fetchedMessage.url)
+          .setStyle(ButtonStyle.Link),
+        new ButtonBuilder()
+          .setCustomId("cancel")
+          .setEmoji("🗑️")
+          .setStyle(ButtonStyle.Secondary)
+      );
 
-        message.channel.send({
-          embeds: [
-            {
-              description: fetchedMessage.content,
-              author: {
-                name: fetchedMessage.author.tag,
-                iconURL: fetchedMessage.author.displayAvatarURL(),
-              },
-              color: 0x4d4df7,
-              timestamp: new Date(fetchedMessage.createdTimestamp),
+      message.channel.send({
+        embeds: [
+          {
+            description: fetchedMessage.content,
+            author: {
+              name: fetchedMessage.author.tag,
+              iconURL: fetchedMessage.author.displayAvatarURL(),
             },
-          ],
-          components: [buttons],
-        });
+            color: 0x4d4df7,
+            timestamp: new Date(fetchedMessage.createdTimestamp),
+          },
+        ],
+        components: [buttons],
+      });
 
-        //メッセージリンクだけが投稿された場合の処理
-        if (url == message.content) {
-          message.delete();
-        }
-      } catch (err) {
-        return;
+      //メッセージリンクだけが投稿された場合の処理
+      if (url == message.content) {
+        message.delete();
       }
+    } catch (err) {
+      return;
     }
-  } catch (err) {
-    err.id = "messageCreate";
-    const errorNotification = require("../errorFunction.js");
-    errorNotification(client, message, err);
   }
 };
