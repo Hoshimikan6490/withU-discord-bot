3662f6a3a3843209a0d6197afa58566aa4bedbea
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-12-03 11:34:59 +0900

    to be active button on DM

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,39 +6,41 @@ const { InteractionType } = require("discord.js");
 const fs = require("fs");
 
 module.exports = async (client, interaction) => {
-  if (!interaction?.guild) {
-    return interaction?.reply({
-      content:
-        "❌ このBOTはサーバー内でのみ動作します。\nお手数をおかけしますが、サーバー内でご利用ください。",
-      ephemeral: true,
-    });
-  } else {
-    if (interaction?.type == InteractionType.ApplicationCommand) {
-      fs.readdir("./commands", (err, files) => {
-        if (err) throw err;
-        files.forEach(async (f) => {
-          let props = require(`../commands/${f}`);
-          if (interaction.commandName == props.name) {
-            try {
-              return props.run(client, interaction);
-            } catch (err) {
-              Sentry.captureException(err);
-              return interaction?.reply({
-                content: `❌ 何らかのエラーが発生しました。`,
-                ephemeral: true,
-              });
-            }
-          }
-        });
+  if (interaction?.type == InteractionType.ApplicationCommand) {
+    if (!interaction?.guild) {
+      return interaction?.reply({
+        content:
+          "❌ このBOTはサーバー内でのみ動作します。\nお手数をおかけしますが、サーバー内でご利用ください。",
+        ephemeral: true,
       });
     }
 
-    if (interaction?.type == InteractionType.MessageComponent) {
-      // button 処理
-      let buttonId = interaction.customId;
-      if (buttonId == "cancel" || buttonId == "delete") {
-        await interaction.message.delete();
-      }
+    fs.readdir("./commands", (err, files) => {
+      if (err) throw err;
+      files.forEach(async (f) => {
+        let props = require(`../commands/${f}`);
+        if (interaction.commandName == props.name) {
+          try {
+            return props.run(client, interaction);
+          } catch (err) {
+            Sentry.captureException(err);
+            return interaction?.reply({
+              content: `❌ 何らかのエラーが発生しました。`,
+              ephemeral: true,
+            });
+          }
+        }
+      });
+    });
+  }
+
+  if (interaction?.type == InteractionType.MessageComponent) {
+    // button 処理
+    let buttonId = interaction.customId;
+    if (buttonId == "guildJoinContinue") {
+      await interaction.reply("OK");
+    } else if (buttonId == "cancel" || buttonId == "delete") {
+      await interaction.message.delete();
     }
   }
 };
