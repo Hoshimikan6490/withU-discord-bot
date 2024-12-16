786d12f084acc276890338b1762ed22d26b75c6b
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-12-16 10:06:30 +0900

    add Sentry error fetch

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,7 @@
+// for using sentry
+require("../instrument");
+const Sentry = require("@sentry/node");
+
 const {
   ActionRowBuilder,
   ButtonBuilder,
@@ -6,24 +10,25 @@ const {
 } = require("discord.js");
 
 module.exports = async (client, member) => {
-  let joinedMember = await client.users.fetch(member.id);
-  // 処罰後の連絡先を決める！
-  let guildJoinContinue = new ActionRowBuilder().addComponents(
-    new ButtonBuilder()
-      .setCustomId("guildJoinContinue")
-      .setEmoji("✅")
-      .setLabel("続ける")
-      .setStyle(ButtonStyle.Success)
-  );
-  let embed1 = new EmbedBuilder()
-    .setTitle("withU サーバーにご参加いただき、ありがとうございます！")
-    .setDescription(
-      "本サーバーは、大学生同士の交流から様々なものづくりをしていくことを目標として、東京国際工科専門職大学(IPUT)を中心に複数の大学の学生が集まっているサーバーです。"
+  try {
+    let joinedMember = await client.users.fetch(member.id);
+    // 処罰後の連絡先を決める！
+    let guildJoinContinue = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("guildJoinContinue")
+        .setEmoji("✅")
+        .setLabel("続ける")
+        .setStyle(ButtonStyle.Success)
     );
-  let embed2 = new EmbedBuilder()
-    .setTitle("ご参加にあたり注意事項")
-    .setDescription(
-      `本サーバーでは、安心・安全な環境づくりを目的としてルールを策定しております。これ以降の参加手続きを続ける場合は、以下のルールに同意したものとみなします。
+    let embed1 = new EmbedBuilder()
+      .setTitle("withU サーバーにご参加いただき、ありがとうございます！")
+      .setDescription(
+        "本サーバーは、大学生同士の交流から様々なものづくりをしていくことを目標として、東京国際工科専門職大学(IPUT)を中心に複数の大学の学生が集まっているサーバーです。"
+      );
+    let embed2 = new EmbedBuilder()
+      .setTitle("ご参加にあたり注意事項")
+      .setDescription(
+        `本サーバーでは、安心・安全な環境づくりを目的としてルールを策定しております。これ以降の参加手続きを続ける場合は、以下のルールに同意したものとみなします。
 まあ、一言でまとめてしまえば__「常識ある言動/行動をしろ」というだけ__ですが、言い争いになってからでは遅いので以下のように定めています。
 なお、このルールおよびそれ以降の参加案内で表示される「発言」という単語にはメッセージ送信やボイスチャットでの発言、画面共有など、すべての発信活動が含まれます。
       \`\`\`
@@ -34,13 +39,16 @@ module.exports = async (client, member) => {
 5. 露骨な宣伝行為は、宣伝行為を許可する旨がチャンネル名やチャンネル概要に書かれているチャンネルでのみ、許可されます。話の流れで「こんなのあるよ～」などのように宣伝する行為は認められますが、全てのチャンネルにおいてチャンネルの趣旨に合わない場合や明らかに高頻度な宣伝行為は処罰の対象になります。
 6. 以上のルールに反した行為を発見した場合、それ相応の処罰(警告やミュート(一定時間の発言禁止処分)、追放やBAN(永久追放))を行う場合があります。処罰に対し、異論がある場合は「」からお問い合わせが可能ですが、十分に検討をした上で処罰を行うため、ご期待に応えられない可能性がございます。予めご了承ください。
 \`\`\``
-    );
-  let embed3 = new EmbedBuilder().setTitle("アンケートご協力のお願い")
-    .setDescription(`本サーバーを安全に運用するため、全参加者様に所属団体と本名をご回答いただき、その内容を参加後のサーバー内で参加者同士で閲覧できる状態にしております。
+      );
+    let embed3 = new EmbedBuilder().setTitle("アンケートご協力のお願い")
+      .setDescription(`本サーバーを安全に運用するため、全参加者様に所属団体と本名をご回答いただき、その内容を参加後のサーバー内で参加者同士で閲覧できる状態にしております。
       \nまた、基本的にすべてのユーザーに所属先の提示を義務付けていますが、本サーバー内での発言はアナウンスチャンネルなどのwithUとしての発信活動を除き、特に表記をしない場合はすべて個人の発言として扱われます。`);
 
-  (await joinedMember).send({
-    embeds: [embed1, embed2, embed3],
-    components: [guildJoinContinue],
-  });
+    (await joinedMember).send({
+      embeds: [embed1, embed2, embed3],
+      components: [guildJoinContinue],
+    });
+  } catch (err) {
+    Sentry.captureException(err);
+  }
 };
