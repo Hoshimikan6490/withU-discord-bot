3dddb86719e6144d208634e30711e9e7109939e0
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-02-09 18:38:05 +0900

    bug fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -19,7 +19,7 @@ const {
   getDatabaseFromSchoolID,
   getDatabaseFromSchoolName,
 } = require("../databaseController");
-const { joinedMemberGuide } = require("./guildMemberAdd");
+const { joinedMemberGuide } = require("../joinedMemberGuide");
 require("dotenv").config();
 
 async function sendJoinProcessLog(client, type, howToSet, userId) {
